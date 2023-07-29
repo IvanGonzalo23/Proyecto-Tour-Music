b418d0e5eeed98cdd5e01e4dcf5bf9d48e2c66ea
Ivan Gonzalo <alzabetivan@gmail.com>
2023-07-29 11:24:36 -0300

    prueba de uso de tkintermapview

---
+++ prueba_evento.py
@@ -0,0 +1,10 @@
+import tkinter
+import tkintermapview
+
+root_tk = tkinter.Tk()
+root_tk.geometry(f"{800}x{600}")
+root_tk.title("map_view_example.py")
+
+
+map_widget = tkintermapview.TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
+map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
